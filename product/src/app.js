7cd41eddf040650d98c4ca4d03af6b2310c412de
Deriel Neto <114691211+deriel-neto@users.noreply.github.com>
2023-02-06 22:25:19 -0300

    rodando servidor e conectando ao mongo db

---
+++ product/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import db from "./config/dbconnect.js";
+
+db.on("error", console.log.bind(console, "Erro de conexão"))
+db.once("open", () => {
+    console.log("conexão feita com sucesso")
+})
+
+const app = express();
+app.use(express.json())
+
+export default app;
