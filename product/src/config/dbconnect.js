22e5ceb557fb7c5ea6b9778abf19a22e9c0c9889
Deriel Neto <114691211+deriel-neto@users.noreply.github.com>
2023-02-13 13:36:33 -0300

    subindo mongo pelo docker

--- product/src/config/dbconnect.js
+++ product/src/config/dbconnect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb://localhost:27017/ecomm");
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://admin:secret@mongodb:27017/ecomm-product?authSource=admin");
 
 let db = mongoose.connection;
 
