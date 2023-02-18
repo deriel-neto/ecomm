6149ca8bfbcc0c351bad43b623663e6b211b6eb5
Deriel Neto <114691211+deriel-neto@users.noreply.github.com>
2023-02-17 21:01:38 -0300

    detalhando pagamento pelo id

--- finance/src/controller/PaymentsController.js
+++ finance/src/controller/PaymentsController.js
@@ -2,12 +2,25 @@ const db = require ('../models/index')
 
 class PaymentController {
 
-    static registerPayment(req, res) {
+    static async registerPayment(req, res) {
         const newPayment = req.body
         try{
-            const newPaymentcreated = db.payments.create(newPayment)
-            return res.status(200).json(newPaymentcreated)
+            const newPaymentcreated = await db.payments.create(newPayment)
+            return res.status(200).location(`/payments/${newPaymentcreated.id}`).json(newPaymentcreated)
         } catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+    static async listPaymentById(req, res){
+        const {id} = req.params
+        try{
+            const payment = await db.payments.findOne({
+                where:{
+                    id: Number(id)
+                }, attributes:{exclude:["cvv"]}
+            })
+            return res.status(200).json(payment)
+        } catch (error) {
             return res.status(500).json(error.message)
         }
     }
--- finance/src/routes/paymentsRoute.js
+++ finance/src/routes/paymentsRoute.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router
     .post("/payments", PaymentController.registerPayment)
+    .get("/payments/:id", PaymentController.listPaymentById)
 
 module.exports = router
