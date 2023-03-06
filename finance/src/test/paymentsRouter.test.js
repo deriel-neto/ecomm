0a33ef68f9c92beb53ac3cb6015288436b575730
Deriel Neto <114691211+deriel-neto@users.noreply.github.com>
2023-03-06 01:39:10 -0300

    corrigindo erro encontrado pelo eslint

--- finance/src/test/paymentsRouter.test.js
+++ finance/src/test/paymentsRouter.test.js
@@ -3,7 +3,6 @@ const {
     afterEach, beforeEach, describe, it,
 } = require('@jest/globals');
 const app = require('../app');
-const { response } = require('../app');
 
 let server;
 beforeEach(() => {
