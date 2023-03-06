357587100ec93d81789784410a7c272316196cf5
Deriel Neto <114691211+deriel-neto@users.noreply.github.com>
2023-03-05 22:22:59 -0300

    concluindo teste de integração nas rotas de categorias

--- product/src/config/dbconnect.js
+++ product/src/config/dbconnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://admin:secret@mongodb:27017/ecomm?authSource=admin');
+mongoose.connect('mongodb://admin:secret@127.0.0.1:27017/ecomm?authSource=admin');
 
 const db = mongoose.connection;
 
---
+++ product/src/test/categoriesRoutes.test.js
@@ -0,0 +1,66 @@
+import request from 'supertest';
+import {
+    afterEach, beforeEach, describe, it,
+} from '@jest/globals';
+import app from '../app';
+
+let server;
+beforeEach(() => {
+    const port = 3000;
+    server = app.listen(port);
+});
+
+afterEach(() => {
+    server.close();
+});
+
+describe('listando categorias', () => {
+    it('deve retosnar uma lista com as categorias', async () => {
+        await request(app)
+            .get('/categories')
+            .expect(200);
+    });
+});
+
+let idNewCategory;
+describe('post em category', () => {
+    it('criando uma nova categorias', async () => {
+        const retorno = await request(app)
+            .post('/categories')
+            .send({
+                nome: 'esport',
+                status: 'ativo',
+            })
+            .expect(201);
+        // eslint-disable-next-line no-underscore-dangle
+        idNewCategory = retorno.body._id;
+    });
+});
+
+describe('listando categoria pelo id', () => {
+    it('deve retosnar uma categoria', async () => {
+        await request(app)
+            .get(`/categories/${idNewCategory}`)
+            .expect(200);
+    });
+});
+
+describe('post em category', () => {
+    it('criando uma nova categorias', async () => {
+        await request(app)
+            .put(`/categories/${idNewCategory}`)
+            .send({
+                nome: 'esport',
+                status: 'inativo',
+            })
+            .expect(200);
+    });
+});
+
+describe('deletando categoria pelo id', () => {
+    it('deve deletar uma categoria', async () => {
+        await request(app)
+            .delete(`/categories/${idNewCategory}`)
+            .expect(200);
+    });
+});
